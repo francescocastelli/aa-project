7b2704d567f3fcdde8a10da16a6f76d2ac254e78
francescocastelli <francesco8.castelli@mail.polimi.it>
2021-07-22 14:28:22 +0200

    added test figure 2 for lexm

---
+++ tests/lexm/test.cpp
@@ -0,0 +1,68 @@
+#include "../../include/graph.h"
+#include "../../include/lexm.h"
+#include <gtest/gtest.h>
+#include <algorithm>
+
+namespace my {
+namespace project {
+namespace {
+
+class GraphTest : public ::testing::Test {
+
+protected:
+  GraphTest() {
+  }
+
+  ~GraphTest() override {
+  }
+
+  void SetUp() override {
+  }
+};
+
+TEST_F(GraphTest, Figure2) {
+	Graph g;
+	std::vector<int> baselineOrder = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	for (int i=1; i<10; ++i)
+	{
+		g.addNode(i);
+	}
+
+	g.addEdge(1, 2);
+	g.addEdge(1, 5);
+
+	g.addEdge(2, 3);
+	g.addEdge(2, 4);
+	g.addEdge(2, 6);
+
+	g.addEdge(3, 7);
+
+	g.addEdge(4, 5);
+	g.addEdge(4, 6);
+	g.addEdge(4, 8);
+
+	g.addEdge(5, 8);
+
+	g.addEdge(6, 7);
+	g.addEdge(6, 9);
+
+	g.addEdge(7, 9);
+
+	g.addEdge(8, 9);
+
+	graph_algorithms::lexm(g);
+
+	auto order = g.getOrder();
+	for(int i=0; i<order.size(); ++i)
+		ASSERT_EQ(order[i], baselineOrder[i]);
+}
+
+}  // namespace
+}  // namespace project
+}  // namespace my
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
