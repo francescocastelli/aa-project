ae7ac25ec3f65d8d327734ecd73bdc5ef03a1669
francescocastelli <francesco8.castelli@mail.polimi.it>
2021-07-29 11:22:20 +0200

    added new graph test for lexm and lexp

--- tests/lexm/test.cpp
+++ tests/lexm/test.cpp
@@ -85,6 +85,163 @@ TEST_F(GraphTest, Figure3) {
 	ASSERT_EQ(numFillEdges, 2) << "num fill edges is: " << numFillEdges;
 }
 
+// this is a perfect elimination graph 
+// so lexm should get to the same result of lexp 
+// since any minimal ordering in a perfect elimination graph is perfect
+TEST_F(GraphTest, PerfectGraph1) {
+	Graph g(8);
+
+	g.addEdge(1, 2);
+	g.addEdge(1, 4);
+	g.addEdge(1, 7);
+
+	g.addEdge(2, 3);
+	g.addEdge(2, 4);
+	g.addEdge(2, 5);
+	g.addEdge(2, 7);
+
+	g.addEdge(3, 5);
+	g.addEdge(3, 7);
+
+	g.addEdge(4, 5);
+	g.addEdge(4, 6);
+	g.addEdge(4, 7);
+
+	g.addEdge(5, 6);
+	g.addEdge(5, 7);
+
+	g.addEdge(6, 7);
+
+	g.addEdge(7, 8);
+
+
+	graph_algorithms::lexm(g);
+
+	auto monAdjSet = g.computeMonAdjSet();
+	int numEdgesBefore = g.getEdgeNumber();
+	int fillCount = 0;
+	graph_algorithms::fill(g, std::move(monAdjSet), true, fillCount);
+	int numFillEdges = g.getEdgeNumber() - numEdgesBefore;
+
+	ASSERT_EQ(numFillEdges, 0) << "num fill edges is: " << numFillEdges;
+}
+
+TEST_F(GraphTest, PerfectGraph2) {
+	Graph g(9);
+
+	g.addEdge(1, 5);
+	g.addEdge(1, 6);
+	g.addEdge(1, 7);
+
+	g.addEdge(2, 3);
+	g.addEdge(2, 9);
+
+	g.addEdge(3, 7);
+	g.addEdge(3, 8);
+	g.addEdge(3, 9);
+
+	g.addEdge(4, 5);
+	g.addEdge(4, 6);
+	g.addEdge(4, 9);
+
+	g.addEdge(5, 6);
+	g.addEdge(5, 7);
+	g.addEdge(5, 9);
+
+	g.addEdge(6, 7);
+	g.addEdge(6, 8);
+	g.addEdge(6, 9);
+
+	g.addEdge(7, 8);
+	g.addEdge(7, 9);
+
+	g.addEdge(8, 9);
+	
+	graph_algorithms::lexm(g);
+
+	auto monAdjSet = g.computeMonAdjSet();
+	int numEdgesBefore = g.getEdgeNumber();
+	int fillCount = 0;
+	graph_algorithms::fill(g, std::move(monAdjSet), true, fillCount);
+	int numFillEdges = g.getEdgeNumber() - numEdgesBefore;
+
+	ASSERT_EQ(numFillEdges, 0) << "num fill edges is: " << numFillEdges;
+}
+
+TEST_F(GraphTest, PerfectGraph3) {
+	Graph g(7);
+
+	g.addEdge(1, 2);
+	g.addEdge(1, 3);
+	g.addEdge(1, 4);
+	g.addEdge(1, 5);
+	g.addEdge(1, 6);
+	g.addEdge(1, 7);
+
+	g.addEdge(2, 3);
+
+	g.addEdge(3, 4);
+
+	g.addEdge(4, 5);
+
+	g.addEdge(5, 6);
+
+	g.addEdge(6, 7);
+
+	graph_algorithms::lexm(g);
+
+	auto monAdjSet = g.computeMonAdjSet();
+	int numEdgesBefore = g.getEdgeNumber();
+	int fillCount = 0;
+	graph_algorithms::fill(g, std::move(monAdjSet), true, fillCount);
+	int numFillEdges = g.getEdgeNumber() - numEdgesBefore;
+
+	ASSERT_EQ(numFillEdges, 0) << "num fill edges is: " << numFillEdges;
+}
+
+// https://www.semanticscholar.org/paper/Minimal-Elimination-Ordering-Inside-a-Given-Chordal-Dahlhaus/b05011bcb117b104ca0efe5b13bf011d73c1977a
+TEST_F(GraphTest, MinimalOrderingGraph) {
+	Graph g(9);
+
+	g.addEdge(1, 3);
+	g.addEdge(1, 4);
+	g.addEdge(1, 5);
+	g.addEdge(1, 6);
+	g.addEdge(1, 7);
+	g.addEdge(1, 8);
+	g.addEdge(1, 9);
+
+	g.addEdge(2, 3);
+	g.addEdge(2, 4);
+	g.addEdge(2, 5);
+	g.addEdge(2, 6);
+	g.addEdge(2, 7);
+	g.addEdge(2, 8);
+	g.addEdge(2, 9);
+
+	g.addEdge(3, 4);
+
+	g.addEdge(4, 5);
+
+	g.addEdge(5, 6);
+
+	g.addEdge(6, 7);
+
+	g.addEdge(7, 8);
+
+	g.addEdge(8, 9);
+
+	graph_algorithms::lexm(g);
+
+	auto monAdjSet = g.computeMonAdjSet();
+	int numEdgesBefore = g.getEdgeNumber();
+	int fillCount = 0;
+	graph_algorithms::fill(g, std::move(monAdjSet), true, fillCount);
+	int numFillEdges = g.getEdgeNumber() - numEdgesBefore;
+
+	ASSERT_EQ(numFillEdges, 15) << "num fill edges is: " << numFillEdges;
+}
+
 }  // namespace
 }  // namespace project
 }  // namespace my
--- tests/lexp/test.cpp
+++ tests/lexp/test.cpp
@@ -54,7 +54,7 @@ TEST_F(GraphTest, Figure4) {
 	ASSERT_EQ(numFillEdges, 0) << "num fill edges is: " << numFillEdges;
 }
 
-TEST_F(GraphTest, PerfectGraph) {
+TEST_F(GraphTest, PerfectGraph1) {
 	Graph g(8);
 
 	g.addEdge(1, 2);
@@ -80,6 +80,79 @@ TEST_F(GraphTest, PerfectGraph) {
 
 	g.addEdge(7, 8);
 
+
+	graph_algorithms::lexp(g);
+
+	auto monAdjSet = g.computeMonAdjSet();
+	int numEdgesBefore = g.getEdgeNumber();
+	int fillCount = 0;
+	graph_algorithms::fill(g, std::move(monAdjSet), true, fillCount);
+	int numFillEdges = g.getEdgeNumber() - numEdgesBefore;
+
+	ASSERT_EQ(numFillEdges, 0) << "num fill edges is: " << numFillEdges;
+}
+
+TEST_F(GraphTest, PerfectGraph2) {
+	Graph g(9);
+
+	g.addEdge(1, 5);
+	g.addEdge(1, 6);
+	g.addEdge(1, 7);
+
+	g.addEdge(2, 3);
+	g.addEdge(2, 9);
+
+	g.addEdge(3, 7);
+	g.addEdge(3, 8);
+	g.addEdge(3, 9);
+
+	g.addEdge(4, 5);
+	g.addEdge(4, 6);
+	g.addEdge(4, 9);
+
+	g.addEdge(5, 6);
+	g.addEdge(5, 7);
+	g.addEdge(5, 9);
+
+	g.addEdge(6, 7);
+	g.addEdge(6, 8);
+	g.addEdge(6, 9);
+
+	g.addEdge(7, 8);
+	g.addEdge(7, 9);
+
+	g.addEdge(8, 9);
+	
+	graph_algorithms::lexp(g);
+
+	auto monAdjSet = g.computeMonAdjSet();
+	int numEdgesBefore = g.getEdgeNumber();
+	int fillCount = 0;
+	graph_algorithms::fill(g, std::move(monAdjSet), true, fillCount);
+	int numFillEdges = g.getEdgeNumber() - numEdgesBefore;
+
+	ASSERT_EQ(numFillEdges, 0) << "num fill edges is: " << numFillEdges;
+}
+
+TEST_F(GraphTest, PerfectGraph3) {
+	Graph g(7);
+
+	g.addEdge(1, 2);
+	g.addEdge(1, 3);
+	g.addEdge(1, 4);
+	g.addEdge(1, 5);
+	g.addEdge(1, 6);
+	g.addEdge(1, 7);
+
+	g.addEdge(2, 3);
+
+	g.addEdge(3, 4);
+
+	g.addEdge(4, 5);
+
+	g.addEdge(5, 6);
+
+	g.addEdge(6, 7);
 
 	graph_algorithms::lexp(g);
 
